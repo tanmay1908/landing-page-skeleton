700879b017d63c4d5bb35e1274eb40a6e6042f11
Tanmay Agarwal <tanmayxagarwal@gmail.com>
2023-11-19 03:26:42 -0800

    icons test 2

--- lp-items.tsx
+++ lp-items.tsx
@@ -1,22 +1,11 @@
+import expertIcon from 'assets/experts.svg';
+
  export const LP_GRID_ITEMS = [
   {
     title: "Curated Content",
     description: "10 minute, hands-on content, created by invite-only industry practioners",
     icon: (
-      <svg
-        xmlns="assets/experts.svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="h-6 w-6"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18"
-        />
-      </svg>
+      <img src={expertIcon} alt="Expert Icon" className="h-6 w-6" />
     ),
   },
 ]
