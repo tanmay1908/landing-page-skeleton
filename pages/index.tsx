c979aa0b3f30df497cb7edde778e66f29d5839c3
Tanmay Agarwal <tanmayxagarwal@gmail.com>
2023-11-19 05:49:02 -0800

    bugfix2

--- pages/index.tsx
+++ pages/index.tsx
@@ -53,7 +53,7 @@ export default function Web() {
           </div>
         </div>
       </section>
-      <section className="bg-white dark:bg-gray-900">
+      {/* <section className="bg-white dark:bg-gray-900">
         <div className="mx-auto grid max-w-screen-xl px-4 py-8 text-center lg:py-16">
           <div className="mx-auto place-self-center">
             <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight dark:text-white md:text-5xl xl:text-6xl">
@@ -64,7 +64,7 @@ export default function Web() {
             </p>
           </div>
         </div>
-      </section>
+      </section> */}
     </>
   )
 }
